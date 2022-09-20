3aececeb918656d245dbc36c12b2d32d1ebf4f7d
Bex Birkett <bex.birkett@outlook.com>
2022-09-20 15:32:13 +0100

    installed mongoose, implemented connectivity to mongodb

--- Application/server/server.js
+++ Application/server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const playlistRoutes = require("./routes/playlists");
 
 // express app
@@ -16,7 +17,15 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/playlists/", playlistRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("listening on port", process.env.PORT);
-});
+// connect to DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to mongodb & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # media-player
+
+Trello Board: https://trello.com/invite/b/qIh5VrLc/02699ea673c8efa79f8f9c72c876c39e/media-player
